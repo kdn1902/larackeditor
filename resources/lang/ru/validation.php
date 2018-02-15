b473dd278b0faae2208a1c67d6fc0cb14115e811
Kosinov Dmitry <kdn1902@gmail.com>
2018-02-15 16:46:36 +0300

    add

---
+++ resources/lang/ru/validation.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+ 		'required' => ':attribute не введена.',
+ 		'captcha' => ':attribute введена не верно.',
+ 		'unique'  => 'Такой :attribute уже есть.',
+ 	    'confirmed' => ':attribute не совпадают.',
+];
